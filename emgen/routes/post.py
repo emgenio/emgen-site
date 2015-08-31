a773e95d9f0c6eaa904d1037e32fdcde34bceed9
Daniel Joshua Holmes <dan@centricwebestate.com>
2015-08-31 17:41:24 +1000

    Don't allow emgen emails...

--- emgen/routes/post.py
+++ emgen/routes/post.py
@@ -1,5 +1,6 @@
 from bottle import request, abort, route
 from ..storage.helpers import create_throwaway
+import re
 
 @route(method='POST')
 def emgen():
@@ -9,6 +10,14 @@ def emgen():
     if not request.json['email']:
         return abort(400, 'Must pass a valid email')
         
+    if not re.match(r"[a-z0-9!#$%&'*+\/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+\/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?", request.json['email']):
+        return abort(400, 'Must pass a valid email')
+
+    if request.json['email'].find('@emgen.io'):
+        return abort(400, 'Must pass a valid email. Not an emgen one you sneaky severus snape.')
+
+
+
     id = create_throwaway(email=request.json['email'])
 
     if not id is False:
